2c7190f982f5d68ec691e2a1828f4fcafc898cf5
OutOffOrder <87262093+OutOffOrder@users.noreply.github.com>
2022-10-05 13:02:32 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+require("dotenv").config()
+const { Client } =require("discord.js")
+
+const client = new Client({intents: []})
+
+client.once("ready", () => {
+    console.log(`Ready! Logged in as ${client.user.tag}! I'm on ${client.guilds.cache.size} guilds`)
+})
+
+
+
+client.login(process.env.DISCORD_BOT_TOKEN)
